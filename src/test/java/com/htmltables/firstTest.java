614579d9927bc8298021c81e824031167b4f05c4
vxkvankai <vankaicurry@gmail.com>
2017-12-26 22:49:11 -0600

    Tried to use our frame work for a tutorial example. 1. Testng.xml: added a parm for baseUrl with ‘demo.guru99.com/selenium/newtours/’ 2. Modified firstTest class to include before and after methods.

--- src/test/java/com/htmltables/firstTest.java
+++ src/test/java/com/htmltables/firstTest.java
@@ -1,38 +1,52 @@
 package com.htmltables;
 
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.pages.tables.TablePages;
+import com.utils.BaseUtils;
+import com.utils.ExcelUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class firstTest {
+public class firstTest extends BaseUtils {
 
+    @BeforeClass(alwaysRun = true)
+    public void launchBrowsers(ITestContext context) throws Exception {
 
-
-    public static void main(String[] args) {
-        // declaration and instantiation of objects/variables
-
-        WebDriver driver = new ChromeDriver();
-
-        String baseUrl = "http://demo.guru99.com/selenium/newtours/";
-        String expectedTitle = "Welcome: Mercury Tours";
-        String actualTitle = "";
-
-        // launch Fire fox and direct it to the Base URL
-        driver.get(baseUrl);
-
-        // get the actual value of the title
-        actualTitle = driver.getTitle();
-
-        /*
-         * compare the actual title of the page with the expected one and print the result as "Passed" or "Failed"
-         */
-        if (actualTitle.contentEquals(expectedTitle)) {
-            System.out.println("Test Passed!");
-        } else {
-            System.out.println("Test Failed");
-        }
-
-        //close Fire fox
-        driver.close();
+        setDriver();
+        NavigatToUrl();
 
     }
-}
+
+    //        WebDriver driver = new ChromeDriver();
+    // String baseUrl = "http://demo.guru99.com/selenium/newtours/";
+    String expectedTitle = "Welcome: Mercury Tours";
+    String actualTitle = "";
+
+
+    //driver.get(baseUrl);
+
+    // get the actual value of the title
+    actualTitle=driver.getTitle();
+
+    /*
+     * compare the actual title of the page with the expected one and print the result as "Passed" or "Failed"
+     */
+    if(actualTitle.contentEquals(expectedTitle))
+    {
+        System.out.println("Test Passed!");
+    }else
+    {
+        System.out.println("Test Failed");
+    }
+
+    @AfterClass(alwaysRun = true)
+        public void terminateApp() {
+            tearDown();
+        }
+
+}}
